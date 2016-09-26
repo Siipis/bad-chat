df1b15916384bae7ef3b8823880fa68f3e681a93
Ami <amalia@varjohovi.net>
2016-09-26 14:55:36 +0300

    Fix bug where description displays as undefined if not found.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1503,6 +1503,14 @@ app.controller('chatController', function ($compile, $scope, $rootScope, $sce, A
         var link = this;
 
         function popover(meta) {
+            if (meta.title === undefined) {
+                meta.title = '[no title found]';
+            }
+
+            if (meta.description === undefined) {
+                meta.description = '[no description found]';
+            }
+
             var template = '<div class="popover" role="tooltip">' +
                 '<div class="arrow"></div>' +
                 '<h3 class="popover-title">' + meta.title + '</h3>' +
